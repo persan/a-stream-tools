a3356001188dbbcf57b59f59a0564a8b47fb8549
persan <per.s.sandberg@bahnhof.se>
2022-01-25 19:02:56 +0100

    Dont run unfinnushed tests

--- tests/src/stream_tools-tests-my_suite.adb
+++ tests/src/stream_tools-tests-my_suite.adb
@@ -1,6 +1,6 @@
 with Stream_Tools.Tests.Memory_Stream_Tests;
 with Stream_Tools.Tests.Bufferd_Stream_Tests;
-with Stream_Tools.Tests.Generic_Classwide_Ring;
+--  with Stream_Tools.Tests.Generic_Classwide_Ring;
 package body Stream_Tools.Tests.My_Suite is
 
    use AUnit.Test_Suites;
@@ -11,12 +11,12 @@ package body Stream_Tools.Tests.My_Suite is
    --  Statically allocate test cases:
    Test_1 : aliased Stream_Tools.Tests.Bufferd_Stream_Tests.Bufferd_Stream_Test;
    Test_2 : aliased Stream_Tools.Tests.Memory_Stream_Tests.Memory_Stream_Test;
-   Test_3 : aliased Stream_Tools.Tests.Generic_Classwide_Ring.Test_Case;
+   --  Test_3 : aliased Stream_Tools.Tests.Generic_Classwide_Ring.Test_Case;
    function Suite return Access_Test_Suite is
    begin
       Add_Test (Result'Access, Test_1'Access);
       Add_Test (Result'Access, Test_2'Access);
-      Add_Test (Result'Access, Test_3'Access);
+--      Add_Test (Result'Access, Test_3'Access);
       return Result'Access;
    end Suite;
 
